cb7877e5d1d7373124fcc5932ab8dd06436ee8ef
subhash <subhash14meena@gmail.com>
2022-03-28 19:16:05 +0530

    update

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k2d=bx0nx5=qo@0w-mouhanxns5o)s(mbkvxmki(6590)j&d-a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
